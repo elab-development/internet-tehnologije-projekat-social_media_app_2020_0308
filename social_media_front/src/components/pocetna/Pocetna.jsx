429106d688cf5e2c0dcf2e82dad8e1a4e5c4bdd0
Anastasija Sekulic <as20200308@student.fon.bg.ac.rs>
2024-01-29 04:13:19 +0100

    Kreirana pocetna stranica i njen dizajn, gde se dohvata random citat o prijateljstvu

--- social_media_front/src/components/pocetna/Pocetna.jsx
+++ social_media_front/src/components/pocetna/Pocetna.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Pocetna.css';
 
-const WelcomePage = () => {
-    
-    return (
-      <>
-      <div>
-        <div className="home-container">
-          
-        </div>
-      </div>
-      </>
+const Pocetna = () => {
+  const [citat, setCitat] = useState(null);
+
+  useEffect(() => {
+    const fetchCitati = async () => {
+      try {
+        const response = await axios.get('https://api.quotable.io/random?tags=Friendship');
+        setCitat(response.data);
+      } catch (error) {
+        console.error('Došlo je do greške prilikom dohvatanja citata:', error);
+      }
+    };
+
+    fetchCitati();
+  }, []);
+
+  return (
+    <div className='pocetna-stranica'>
+    <div className="pocetna-tekst">
+      <h1>Dobrodošli na našu društvenu mrežu!</h1>
+      <p>Ovde možete deliti svoje misli, pregledati postove drugih korisnika i pronaći nove prijatelje.</p>
       
-    );
-  
-  };
-  
-  export default WelcomePage;
+      <div className='citat'>
+      <h2>Neki od naših omiljenih citata o prijateljstvu:</h2>
+      {citat && (
+        <blockquote>
+          <p>"{citat.content}"</p>
+          <footer>- {citat.author}</footer>
+        </blockquote>
+      )}
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Pocetna;
